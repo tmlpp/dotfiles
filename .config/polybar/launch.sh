3274f4b90ce8460694d5b6c7257ab7e064fb2cd2
tmlpp <teemu.lappi@gmail.com>
2024-01-22 07:57:31 +0200

    Polybar things

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bottom 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
