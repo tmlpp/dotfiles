ee32ceb50d30090571dec228da31a9efee7a3cb1
tmlpp <teemu.lappi@gmail.com>
2024-01-26 17:49:59 +0200

    nvim: Add setups for lsps

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -4,7 +4,16 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require('lspconfig')
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
       lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+      lspconfig.tsserver.setup({
         capabilities = capabilities
       })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
