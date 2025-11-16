41c9c03699eff79b59c806b83063db1319767e13
tmlpp <teemu.lappi@gmail.com>
2025-11-16 02:24:27 +0200

    nvim: Update lsp thingy for Nvim 0.11 and whatnot

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -3,31 +3,17 @@ return {
     'neovim/nvim-lspconfig',
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lspconfig = require('lspconfig')
-      lspconfig.clangd.setup({
-        capabilities = capabilities
-      })
-      lspconfig.intelephense.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities
-      })
+		vim.lsp.config("*", {})
+		vim.lsp.enable({
+			"lua_ls",
+			"clangd",
+			"solargraph",
+			"intelephense",
+			"pyright",
+			"ts_ls",
+			"gopls",
+			"rust_analyzer",
+		})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
